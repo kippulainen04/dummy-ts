bc63cd4c913381d86ee34446182764a8cac25484
Nhan Phan <kippulainenko@gmail.com>
2022-06-19 14:52:20 +0300

    fetch userinput to console

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,19 @@
+// Validation
+
+// Autobind decorator
+function autobind(_: any, _2: string, descriptor: PropertyDescriptor) {
+    const originalMethod = descriptor.value;
+    const adjDescriptor: PropertyDescriptor = {
+      configurable: true,
+      get() {
+        const boundFn = originalMethod.bind(this);
+        return boundFn;
+      }
+    };
+    return adjDescriptor;
+  }
+
+// ProjectInput CLass
 class ProjectInput {
     templateElement: HTMLTemplateElement;
     hostElement: HTMLDivElement;
@@ -14,21 +30,57 @@ class ProjectInput {
         this.element = importedNode.firstElementChild as HTMLFormElement;
         this.element.id = 'user-input';
 
-        this.titleInputEl = this.element.querySelector('#title') as HTMLInputElement;
-        this.descriptionInputEl = this.element.querySelector('#description') as HTMLInputElement;
-        this.peopleInputEl = this.element.querySelector('#people') as HTMLInputElement;
+        this.titleInputEl = this.element.querySelector(
+            '#title'
+          ) as HTMLInputElement;
+          this.descriptionInputEl = this.element.querySelector(
+            '#description'
+          ) as HTMLInputElement;
+          this.peopleInputEl = this.element.querySelector(
+            '#people'
+          ) as HTMLInputElement;
 
         this.configure();
         this.attach();
     }
+
+    private gatherUserInput(): [string, string, number] | void {
+        const enteredTitle = this.titleInputEl.value;
+        const enteredDescription = this.descriptionInputEl.value;
+        const enteredPeople = this.peopleInputEl.value;
+    
+        if (
+          enteredTitle.trim().length === 0 ||
+          enteredDescription.trim().length === 0 ||
+          enteredPeople.trim().length === 0
+        ) {
+          alert('Invalid input, please try again!');
+          return;
+        } else {
+          return [enteredTitle, enteredDescription, +enteredPeople];
+        }
+    }
+
+    private clearInputs() {
+        this.titleInputEl.value = '';
+        this.descriptionInputEl.value = '';
+        this.peopleInputEl.value = '';
+    }
+
     // error here cuz this inside submitHandler doesnt point to the class, but to current target of the Event
+    @autobind
     private submitHandler(event: Event) {
         event.preventDefault();
-        console.log(this.titleInputEl.value);
+        const userInput = this.gatherUserInput();
+        if (Array.isArray(userInput)) {
+            const [title, desc, people] = userInput;
+            console.log(title, desc, people);
+            this.clearInputs();
+        }
     }
 
     private configure() {
-        this.element.addEventListener('submit', this.submitHandler.bind(this));
+        this.element.addEventListener('submit', this.submitHandler);
     }
 
     private attach() {
