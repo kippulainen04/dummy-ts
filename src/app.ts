d526ae5b4fcda0cc3d5517b48fdcb7c9b8a59795
Nhan Phan <kippulainenko@gmail.com>
2022-06-20 16:40:57 +0300

    finish drag and drop

--- src/app.ts
+++ src/app.ts
@@ -59,10 +59,24 @@ class ProjectState extends State<Project>{
             ProjectStatus.Active
             );
         this.projects.push(newProject);
+        this.updateListerners();
+    }    
+
+    moveProject(projectId: string, newStatus: ProjectStatus) {
+        const project = this.projects.find(prj => prj.id === projectId);
+        if(project && project.status !== newStatus) {
+            project.status = newStatus;
+            this.updateListerners();
+        }
+    }
+
+    private updateListerners () {
         for(const listernerFn of this.listeners){
             listernerFn(this.projects.slice());
         }
-    }    
+    }
+
+
 }
 
 const projectState = ProjectState.getInstance();
@@ -173,7 +187,8 @@ implements Draggable {
 
     @autobind
     dragStartHandler(event: DragEvent) {
-        console.log(event)
+        event.dataTransfer!.setData('text/plain', this.project.id);
+        event.dataTransfer!.effectAllowed = 'move';
     }
 
     dragEndHandler(_: DragEvent) {
@@ -206,16 +221,22 @@ class ProjectList extends Component<HTMLDivElement, HTMLElement>
     }
 
     @autobind
-    dragOverHandler(_: DragEvent) {
-        const listEl = this.element.querySelector('ul')!;
-        listEl.classList.add('droppable')
-
+    dragOverHandler(event: DragEvent) {
+        if(event.dataTransfer && event.dataTransfer.types[0] === 'text/plain') {
+            event.preventDefault();
+            const listEl = this.element.querySelector('ul')!;
+            listEl.classList.add('droppable')
+        }
     }
 
-    dropHandler(_: DragEvent) {
-        
+    @autobind
+    dropHandler(event: DragEvent) {
+        const prjId = event.dataTransfer!.getData('text/plain');
+        projectState.moveProject(prjId, this.type === 'active' 
+        ? ProjectStatus.Active 
+        : ProjectStatus.Finished);
     }
-    
+
     @autobind
     dragLeaveHandler(_: DragEvent) {
         const listEl = this.element.querySelector('ul')!;
